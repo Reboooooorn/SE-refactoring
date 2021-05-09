c6247250614478ea464ee62beaea5b11566292fb
Jegor <georg.reichert.1998@gmail.com>
2021-05-09 20:39:55 +0200

    totalCharge and totalFrequentRenterPoints got their own methods

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -20,23 +20,39 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentalsEnum = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n"
                       + "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (rentalsEnum.hasMoreElements()) {
             Rental currentRental = (Rental) rentalsEnum.nextElement();
-            frequentRenterPoints += currentRental.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + currentRental.getMovie().getTitle() + "\t" + "\t" + currentRental.getDaysRented() + "\t" + String.valueOf(currentRental.getAmount()) + "\n";
-            totalAmount += currentRental.getAmount();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + getTotalAmount() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
         return result;
+    }
+
+    private double getTotalAmount() {
+        double totalAmount = 0;
+        Enumeration rentalsEnum = rentals.elements();
+        while (rentalsEnum.hasMoreElements()) {
+            Rental rental = (Rental) rentalsEnum.nextElement();
+            totalAmount += rental.getAmount();
+        }
+        return totalAmount;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int totalFrequentRenterPoints = 0;
+        Enumeration rentalsEnum = rentals.elements();
+        while (rentalsEnum.hasMoreElements()) {
+            Rental rental = (Rental) rentalsEnum.nextElement();
+            totalFrequentRenterPoints += rental.getFrequentRenterPoints();
+        }
+        return totalFrequentRenterPoints;
     }
 
     public String htmlStatement() {
